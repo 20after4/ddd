1eee2762bf6e32a81a460c2d0f34c321addaa64d
Mukunda Modell <mmodell@wikimedia.org>
2021-10-27 22:05:34 -0500

    svg icons + rollup build step

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    "svgstore": {
+      "options": {
+        "prefix" : "icon-"
+      },
+      "default": {
+        "files": {
+          "www/static/icons.svg": ["www/static/icons/*.svg"]
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-svgstore');
+  grunt.registerTask('default', ['svgstore']);
+}
